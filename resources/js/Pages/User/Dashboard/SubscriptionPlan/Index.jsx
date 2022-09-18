b06c81672efb433718f500241efa23dfbac79dbb
cdngwthkhosy <you@example.com>
2022-09-18 09:35:02 +0700

    feat: integrate midtrans

--- resources/js/Pages/User/Dashboard/SubscriptionPlan/Index.jsx
+++ resources/js/Pages/User/Dashboard/SubscriptionPlan/Index.jsx
@@ -1,15 +1,49 @@
 import Authenticated from "@/Layouts/Authenticated/Index";
 import SubscriptionCard from "@/Components/SubscriptionCard";
 import { Inertia } from "@inertiajs/inertia";
+import { Head } from "@inertiajs/inertia-react";
 
-export default function SubscriptionPlan({ auth, subscriptionPlans }) {
-    const selectSubscription = id => {
-      Inertia.post(route('user.dashboard.subscriptionPlan.userSubscribe', {subscriptionPlan: id}))
-    }
+export default function SubscriptionPlan({ auth, subscriptionPlans, env }) {
+    const selectSubscription = (id) => {
+        Inertia.post(
+            route("user.dashboard.subscriptionPlan.userSubscribe", {
+                subscriptionPlan: id,
+            }),
+            {},
+            {
+                only: ["userSubscription"],
+                onSuccess: ({ props }) =>
+                    onSnapMidtrans(props.userSubscription),
+            }
+        );
+    };
+
+    const onSnapMidtrans = (userSubscription) => {
+        snap.pay(userSubscription.snap_token, {
+            // Optional
+            onSuccess: function (result) {
+                Inertia.visit(route('user.dashboard.index'));
+            },
+            // Optional
+            onPending: function (result) {
+                console.log({ result });
+            },
+            // Optional
+            onError: function (result) {
+                console.log({ result });
+            },
+        });
+    };
 
     return (
         <>
             <Authenticated auth={auth}>
+                <Head title="Subcsription Plan">
+                    <script
+                        src="https://app.sandbox.midtrans.com/snap/snap.js"
+                        data-client-key={env.MIDTRANS_CLIENTKEY}
+                    ></script>
+                </Head>
                 <div className="py-20 flex flex-col items-center">
                     <div className="text-black font-semibold text-[26px] mb-3">
                         Pricing for Everyone
@@ -26,11 +60,15 @@ export default function SubscriptionPlan({ auth, subscriptionPlans }) {
                             <SubscriptionCard
                                 name={subscriptionPlan.name}
                                 price={subscriptionPlan.price}
-                                durationInMonth={subscriptionPlan.active_period_in_months}
+                                durationInMonth={
+                                    subscriptionPlan.active_period_in_months
+                                }
                                 features={JSON.parse(subscriptionPlan.features)}
-                                isPremium={subscriptionPlan.name === 'Premium'}
+                                isPremium={subscriptionPlan.name === "Premium"}
                                 key={subscriptionPlan.id}
-                                onSelectSubscription={() => selectSubscription(subscriptionPlan.id)}
+                                onSelectSubscription={() =>
+                                    selectSubscription(subscriptionPlan.id)
+                                }
                             />
                         ))}
                     </div>
